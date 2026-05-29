3db49b897d83acd7dc3d60af0b0f1d01241eb61a
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-05-29 16:11:15 +0530

    train.py

---
+++ TemporalDifference/train.py
@@ -0,0 +1,47 @@
+
+from TemporalDifference.montecarlo import MonteCarlo
+from TemporalDifference.sarsa import SARSA, NStepSARSA
+from TemporalDifference.qlearning import QLearning
+from TemporalDifference.util import show_cliffwalking_result
+from environment.clifwalk_env import CliffWalkingWithoutPEnv
+from TemporalDifference.dynaq import DynaQ
+
+
+if __name__ == '__main__':
+    SEED = 0
+    ncol = 12
+    nrow = 4
+    env = CliffWalkingWithoutPEnv(ncol=ncol, nrow=nrow)
+    epsilon = 0.1
+    alpha = 0.1
+    gamma = 0.9
+    num_episodes = 500
+    n_actions = 4
+    n_planning = 2
+    agent_mc = MonteCarlo(env=env, gamma=gamma, alpha=alpha, epsilon=epsilon,
+                          n_actions=n_actions, num_episodes=num_episodes, seed=SEED)
+
+    agent_sarsa = SARSA(env=env, gamma=gamma, alpha=alpha, epsilon=epsilon, n_actions=n_actions, num_episodes=num_episodes,
+                        seed=SEED)
+    agent_nsarsa = NStepSARSA(env=env, gamma=gamma, alpha=alpha, epsilon=epsilon, n_actions=n_actions, n_step=5,
+                              num_episodes=num_episodes, seed=SEED)
+    agent_q = QLearning(env=env, gamma=gamma, alpha=alpha, epsilon=epsilon, n_actions=n_actions, num_episodes=num_episodes,
+                        seed=SEED)
+    agent_dynaq = DynaQ(env=env, gamma=gamma, alpha=alpha, epsilon=epsilon, n_planning=n_planning, n_actions=n_actions,
+                        num_episodes=num_episodes, seed=SEED)
+
+    agent_mc.train()
+    show_cliffwalking_result(agent=agent_mc, env=env, plot_title="Monte Carlo")
+
+    agent_sarsa.train()
+    show_cliffwalking_result(agent=agent_sarsa, env=env, plot_title="SARSA")
+
+    agent_nsarsa.train()
+    show_cliffwalking_result(
+        agent=agent_nsarsa, env=env, plot_title="NStepSARSA")
+
+    agent_q.train()
+    show_cliffwalking_result(agent=agent_q, env=env, plot_title="QLearning")
+
+    agent_dynaq.train()
+    show_cliffwalking_result(agent=agent_dynaq, env=env, plot_title="DynaQ")
